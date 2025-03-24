32e67595fb6f7d9055a22d7f9680a9467e9376c8
Daniil Zhuravlev <nakrovati@outlook.com>
2025-03-25 03:02:44 +0400

    test: remove manual loading of modules

--- src/test/kotlin/org/kotatsu/ApplicationTest.kt
+++ src/test/kotlin/org/kotatsu/ApplicationTest.kt
@@ -4,15 +4,11 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import org.kotatsu.plugins.configureRouting
 import kotlin.test.*
 
 class ApplicationTest {
 	@Test
 	fun testRoot() = testApplication {
-		application {
-			configureRouting()
-		}
 		client.get("/").apply {
 			assertEquals(HttpStatusCode.OK, status)
 			assertEquals("Alive", bodyAsText())
