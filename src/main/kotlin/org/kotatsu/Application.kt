e9d93a55134ec0245cffaa962c09e7d8594e2638
Daniil Zhuravlev <nakrovati@outlook.com>
2025-07-23 18:29:48 +0400

    fix: connection pool leak in useTransaction with suspend calls

--- src/main/kotlin/org/kotatsu/Application.kt
+++ src/main/kotlin/org/kotatsu/Application.kt
@@ -1,9 +1,11 @@
 package org.kotatsu
 
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
 import org.kotatsu.plugins.*
 import org.ktorm.database.Database
 
+lateinit var hikariDataSource: HikariDataSource
 lateinit var database: Database
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
--- src/main/kotlin/org/kotatsu/routes/HistoryRoutes.kt
+++ src/main/kotlin/org/kotatsu/routes/HistoryRoutes.kt
@@ -4,6 +4,8 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 import io.ktor.server.auth.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.kotatsu.database
 import org.kotatsu.model.history.HistoryPackage
 import org.kotatsu.plugins.currentUser
@@ -14,30 +16,39 @@ import org.ktorm.database.TransactionIsolation
 fun Route.historyRoutes() {
 	authenticate("auth-jwt") {
 		get("/resource/history") {
-			database.useTransaction(TransactionIsolation.READ_COMMITTED) {
-				val user = call.currentUser
-				if (user == null) {
-					call.respond(HttpStatusCode.Unauthorized)
-					return@get
-				}
-				val response = syncHistory(user, null)
-				call.respond(response)
+			val user = call.currentUser
+			if (user == null) {
+				call.respond(HttpStatusCode.Unauthorized)
+				return@get
 			}
+
+			val response = withContext(Dispatchers.IO) {
+				database.useTransaction(TransactionIsolation.READ_COMMITTED) {
+					syncHistory(user, null)
+				}
+			}
+
+			call.respond(response)
 		}
 		post<HistoryPackage>("/resource/history") { request ->
-			database.useTransaction(TransactionIsolation.READ_COMMITTED) {
-				val user = call.currentUser
-				if (user == null) {
-					call.respond(HttpStatusCode.Unauthorized)
-					return@post
+			val user = call.currentUser
+			if (user == null) {
+				call.respond(HttpStatusCode.Unauthorized)
+				return@post
+			}
+
+			val response = withContext(Dispatchers.IO) {
+				database.useTransaction(TransactionIsolation.READ_COMMITTED) {
+					val result = syncHistory(user, request)
+					user.setHistorySynchronized(System.currentTimeMillis())
+					result
 				}
-				val response = syncHistory(user, request)
-				user.setHistorySynchronized(System.currentTimeMillis())
-				if (response.contentEquals(request)) {
-					call.respond(HttpStatusCode.NoContent)
-				} else {
-					call.respond(response)
-				}
+			}
+
+			if (response.contentEquals(request)) {
+				call.respond(HttpStatusCode.NoContent)
+			} else {
+				call.respond(response)
 			}
 		}
 	}
