ff8dfedac78869f08656bf3eea63c185ff84ccc5
Daniil Zhuravlev <nakrovati@outlook.com>
2025-07-30 19:02:06 +0400

    feat: password hashing with argon2

--- src/main/kotlin/org/kotatsu/resources/User.kt
+++ src/main/kotlin/org/kotatsu/resources/User.kt
@@ -3,12 +3,13 @@ package org.kotatsu.resources
 import org.kotatsu.database
 import org.kotatsu.model.user.*
 import org.kotatsu.model.users
-import org.kotatsu.util.md5
 import org.kotatsu.util.withRetry
 import org.ktorm.dsl.eq
 import org.ktorm.dsl.insertAndGenerateKey
 import org.ktorm.entity.find
 import org.ktorm.entity.singleOrNull
+import org.kotatsu.util.PasswordHasher
+import org.kotatsu.util.md5
 
 suspend fun getOrCreateUser(request: AuthRequest, allowNewRegister: Boolean): UserInfo? = withRetry {
 	require(request.password.length in 2..24) {
@@ -17,15 +18,37 @@ suspend fun getOrCreateUser(request: AuthRequest, allowNewRegister: Boolean): Us
 	require(request.email.length in 5..320 && '@' in request.email) {
 		"Invalid email address"
 	}
-	val passDigest = request.password.md5()
+
 	val user = database.users.find { (it.email eq request.email) }
+
 	when {
-		user == null && allowNewRegister -> registerUser(request, passDigest)
+		user == null && allowNewRegister -> {
+			val bcryptHash = PasswordHasher.hash(request.password)
+			registerUser(request, bcryptHash)
+		}
 		user == null -> null
-		user.passwordHash == passDigest -> user.toUserInfo()
-		else -> null
+		else -> {
+			val storedHash = user.passwordHash
+
+			when {
+				PasswordHasher.isArgon2Hash(storedHash) -> {
+					if (PasswordHasher.verify(request.password, storedHash)) {
+						user.toUserInfo()
+					} else null
+				}
+				else -> { // MD5 legacy
+					if (storedHash == request.password.md5()) {
+						// Upgrade to bcrypt
+						user.passwordHash = PasswordHasher.hash(request.password)
+						user.flushChanges()
+						user.toUserInfo()
+					} else null
+				}
+			}
+		}
 	}
 }
+
 
 private fun registerUser(request: AuthRequest, passwordDigest: String): UserInfo? {
 	val userId = database.insertAndGenerateKey(UsersTable) {
