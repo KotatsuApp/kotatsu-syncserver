25f21460d779d651b5d12e1c767e900e6bef9836
Daniil Zhuravlev <nakrovati@outlook.com>
2025-10-08 18:42:52 +0400

    BREAKING CHANGE(db): format migrations

--- src/main/resources/db/migration/V1__create_users.sql
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
-	id BIGINT AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(320) NOT NULL,
-	password_hash VARCHAR(128) NOT NULL,
-	nickname VARCHAR(100),
-	favourites_sync_timestamp BIGINT,
-	history_sync_timestamp BIGINT,
-	CONSTRAINT uq_users_email UNIQUE (email)
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(320) NOT NULL,
+    password_hash VARCHAR(128) NOT NULL,
+    nickname VARCHAR(100),
+    favourites_sync_timestamp BIGINT,
+    history_sync_timestamp BIGINT,
+    CONSTRAINT uq_users_email UNIQUE (email)
 );
